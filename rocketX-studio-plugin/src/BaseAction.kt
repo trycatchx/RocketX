b2f8817ed95ff49d12ad9ac8b4b861c90074c21f
liuzhaotian <zhaotian.liu@tcl.com>
2021-11-12 15:15:29 +0800

    调整图标

--- rocketX-studio-plugin/src/BaseAction.kt
+++ rocketX-studio-plugin/src/BaseAction.kt
@@ -3,7 +3,11 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.project.Project
 import javax.swing.Icon
-
+/**
+ * @description: 基类
+ * @author: louis
+ * @date: 2020/11/5 3:50 PM
+ */
 abstract class BaseAction(val icon: Icon) : AnAction(icon){
 
     lateinit var mProject: Project
