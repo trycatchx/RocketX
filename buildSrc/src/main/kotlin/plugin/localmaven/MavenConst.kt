2e02bc5ae719e65322b7585dbd9a42137682b5c0
李智勇 <zhiyong18.li@tcl.com>
2021-11-15 15:05:52 +0800

    feat:项目LocalMaven配置改成代码内集成

--- buildSrc/src/main/kotlin/plugin/localmaven/MavenConst.kt
+++ buildSrc/src/main/kotlin/plugin/localmaven/MavenConst.kt
@@ -9,6 +9,8 @@ import java.net.URI
 val Project.MAVEN_LOCAL: URI
     get() = File(rootDir, "repos").toURI()
 
+const val MAVEN_LOCAL_NAME: String = "rocketXLocalMaven"
+
 const val POM_LICENCE_NAME = "Apache-2.0 License"
 const val POM_LICENCE_URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
 
