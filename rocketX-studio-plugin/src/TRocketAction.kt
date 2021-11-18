706eaa81fad2ba0186ebdd6faffc3293fb8cd12e
liuzhaotian <zhaotian.liu@tcl.com>
2021-11-18 11:24:38 +0800

    增加初始化 更新icon状态的功能

--- rocketX-studio-plugin/src/TRocketAction.kt
+++ rocketX-studio-plugin/src/TRocketAction.kt
@@ -1,5 +1,7 @@
-import icons.PluginIcons.ICON_ACTION_ENABLE
-import icons.PluginIcons.ICON_ACTION_UNABLE
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.ui.Messages
+import icons.PluginIcons
 import java.io.File
 
 /**
@@ -7,15 +9,37 @@ import java.io.File
  * @author: louis
  * @date: 2020/11/5 3:58 PM
  */
-class TRocketAction : BaseAction(ICON_ACTION_UNABLE) {
+open class TRocketAction : BaseAction(icons.PluginIcons.ICON_ACTION_ENABLE) {
+
+    private var init = false
+
+
+    //进入 project 需要更新 icon
+    override fun update(e: AnActionEvent) {
+        if (!init) {
+            val project = e.getData(PlatformDataKeys.PROJECT)
+            project?.let {
+                init = true
+                val parentDir = project?.baseDir?.path + "/.gradle/"
+                val targetFile = File(parentDir, "rocketXEnable")
+                if (targetFile.exists()) {
+                    templatePresentation.icon = PluginIcons.ICON_ACTION_UNABLE
+                } else {
+                    templatePresentation.icon = PluginIcons.ICON_ACTION_ENABLE
+                }
+            }
+        }
+    }
+
 
     override fun doAction() {
+
         val parentDir = File(mParentPath)
-        if(!parentDir.exists()) {
+        if (!parentDir.exists()) {
             parentDir.mkdirs()
         }
-        val targetFile =  File(parentDir,"rocketXEnable")
-        if(targetFile.exists()) {
+        val targetFile = File(parentDir, "rocketXEnable")
+        if (targetFile.exists()) {
             // 开启状态
             changeIcon(false)
             targetFile.delete()
@@ -27,12 +51,13 @@ class TRocketAction : BaseAction(ICON_ACTION_UNABLE) {
         }
     }
 
-    private fun changeIcon(enable:Boolean) {
+    fun changeIcon(enable:Boolean) {
         if(enable) {
-            mEvent.presentation.icon = ICON_ACTION_UNABLE
+            mEvent.presentation.icon = PluginIcons.ICON_ACTION_UNABLE
         } else {
-            mEvent.presentation.icon = ICON_ACTION_ENABLE
+            mEvent.presentation.icon = PluginIcons.ICON_ACTION_ENABLE
         }
     }
+
 
 }
